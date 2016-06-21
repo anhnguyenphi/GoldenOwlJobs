8fcaf3ee32d3b3f3cbedcc8d0dae9b7934e69df2
An Luu <lta.anluu@gmail.com>
2016-06-21 16:22:13 +0700

    Edit model Job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,9 @@
 class Job < ActiveRecord::Base
+	validates :name, presence: true
+	validates :detail, presence: true
+	validates :requirement, presence: true
+	validates :offer, presence: true
+
+
+	default_scope -> { order(created_at: :desc) }
 end
