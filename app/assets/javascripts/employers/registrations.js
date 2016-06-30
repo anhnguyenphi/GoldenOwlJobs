4fade13a67698a5ce93824a3a6667e59b3134807
fanNPA <phianhnpa1995@gmail.com>
2016-06-30 09:32:07 +0700

    add image

---
+++ app/assets/javascripts/employers/registrations.js
@@ -0,0 +1,8 @@
+
+$("#add_new_image").click(function() {
+	var num = $(".image_upload").size();
+	var new_input = $(".image_upload").last().clone().attr("class", "image_upload");
+	new_input.children("input").attr("name", "employer[images_attributes][" + num + "][file]")
+														 .attr("id", "employer_images_attributes_" + num + "_file");
+	$("#images_upload").append(new_input);
+});
