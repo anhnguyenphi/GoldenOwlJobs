483dce553f7f5d0247f858b63b445417e2531995
fanNPA <phianhnpa1995@gmail.com>
2016-07-04 10:33:08 +0700

    hha

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,11 +1,3 @@
 class EmployeesController < ApplicationController
-	def apply
-		ap = ApplyRelationship.new(apply_params)
-		render plain: apply_params
-	end
 
-	private
-		def apply_params
-			params.permit(:employee_id, :job_id)
-		end
 end
